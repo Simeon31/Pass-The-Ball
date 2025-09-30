9bff4cf670fe91b5a9e25b97ebf8da5f27c3d3de
Simeon M <simionsimion568@gmail.com>
2025-09-30 20:32:42 +0200

    setting up envirement and adding models

---
+++ resources/js/components/AppLogo.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import AppLogoIcon from '@/components/AppLogoIcon.vue';
+</script>
+
+<template>
+    <div
+        class="flex aspect-square size-8 items-center justify-center text-sidebar-primary-foreground"
+    >
+        <AppLogoIcon class="fill-current text-white dark:text-black" />
+    </div>
+    <div class="ml-3 grid flex-1 text-left text-sm">
+        <span class="mb-0.5 truncate leading-tight font-semibold"
+            >Pass The Ball</span
+        >
+    </div>
+</template>
